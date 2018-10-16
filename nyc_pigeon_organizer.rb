513a25ef7fe5f24b163521fce9062f2b33513a0a
Jacob Menashi <jbmenashi@gmail.com>
2018-10-16 04:00:36 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,7 +20,7 @@ def nyc_pigeon_organizer(data)
       birds.each do |bird|
         if pigeon_list.key?(bird)
           if pigeon_list[bird].key?(category)
-            
+            pigeon_list[bird][category] << indiv_trait.to_s
         else
           bird[category] = {}
         end
