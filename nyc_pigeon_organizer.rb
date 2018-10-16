f68be4091f51860915a0b52050ccf607622422e9
Jacob Menashi <jbmenashi@gmail.com>
2018-10-16 03:24:56 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,17 @@
 require 'pry'
-        :color => {
-          :purple => ["Theo", "Peter Jr.", "Lucky"],
-          :grey => ["Theo", "Peter Jr.", "Ms. K"],
-          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-          :brown => ["Queenie", "Alex"]},
-        :gender => {
-          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-          :female => ["Queenie", "Ms. K"]},
-        :lives => {
-          "Subway" => ["Theo", "Queenie"],
-          "Central Park" => ["Alex", "Ms. K", "Lucky"],
-          "Library" => ["Peter Jr."],
-          "City Hall" => ["Andrew"]}
+        # :color => {
+        #   :purple => ["Theo", "Peter Jr.", "Lucky"],
+        #   :grey => ["Theo", "Peter Jr.", "Ms. K"],
+        #   :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+        #   :brown => ["Queenie", "Alex"]},
+        # :gender => {
+        #   :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+        #   :female => ["Queenie", "Ms. K"]},
+        # :lives => {
+        #   "Subway" => ["Theo", "Queenie"],
+        #   "Central Park" => ["Alex", "Ms. K", "Lucky"],
+        #   "Library" => ["Peter Jr."],
+        #   "City Hall" => ["Andrew"]}
 
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
