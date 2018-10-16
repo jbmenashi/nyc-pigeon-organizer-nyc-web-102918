62d46c519e9e707f40edc5f47a56fb80ec9f106e
Jacob Menashi <jbmenashi@gmail.com>
2018-10-16 03:23:17 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
         :color => {
           :purple => ["Theo", "Peter Jr.", "Lucky"],
           :grey => ["Theo", "Peter Jr.", "Ms. K"],
@@ -15,5 +15,6 @@
 
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  
+  data.each do |category, trait|
+    trait.each do |
 end
