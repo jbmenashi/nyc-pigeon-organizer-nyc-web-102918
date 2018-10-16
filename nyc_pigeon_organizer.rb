5bc24509ab4f0ecddfae56593c03b05d801c10b0
Jacob Menashi <jbmenashi@gmail.com>
2018-10-16 03:56:40 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,8 +18,10 @@ def nyc_pigeon_organizer(data)
   data.each do |category, trait|
     trait.each do |indiv_trait, birds|
       birds.each do |bird|
-        pigeon_list[bird] = category
-        binding.pry
+        if pigeon_list.keys.include?(bird)
+          bird[category] = indiv_trait
+        else
+          bird[category] = {}
       end
     end
   end
